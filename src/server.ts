93c4a3e2ea5ac42adb759a8a2ea1b48b1c52efd1
tarikceylan <tarikceylan@outlook.com.tr>
2025-12-09 19:26:00 +0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { connectDatabase } from './database/connect';
 import { BlogRoutes } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT;
 
 connectDatabase();
